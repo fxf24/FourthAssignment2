cce772adaa2e84d50413c84a2a4d16c5880fae3c
Sam Kim <fxf24@naver.com>
2017-03-23 18:57:38 +0900

    Start again

---
+++ app/src/main/java/com/example/qudqj_000/fourth_assignment/MenuActivity.java
@@ -0,0 +1,15 @@
+package com.example.qudqj_000.fourth_assignment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+
+
+}
